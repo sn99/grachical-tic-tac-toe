dc7854d92d20f8b6c37f89426eb51f4aa12e0c33
sn99 <siddharthn.099@gmail.com>
2018-09-11 16:21:24 +0530

    It spins *sigh...*

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+pub struct App {
+    gl: GlGraphics,
+    rotation: f64,
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(GREEN, gl);
+
+            let transform = c.transform.trans(x, y).rot_rad(rotation).trans(-25.0, -25.0);
+
+            rectangle(RED, square, transform, gl);
+        })
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        self.rotation += 2.0 * args.dt;
+    }
+}
+
 fn main() {
-    println!("Let's start !!!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("Spinning-Square", [200, 200])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build().unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
+    }
 }
